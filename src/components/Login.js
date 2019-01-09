e721e1d2de34f9da02771671e109e0428e303e4c
Killuah15 <Rockingrob@web.de>
2019-01-09 22:58:06 +0100

    Login work
    connected Login Mutation with the UI, added react-spinners-kit module for some fancy loading animations :+1:  hihi

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,44 @@
 import React from "react";
 import { Button, Grid, Row, Col, Clearfix } from "react-bootstrap";
 import { NavLink } from 'react-router-dom';
+import { Mutation, renderToStringWithData } from 'react-apollo';
+import { LOGIN } from '../apollo/templates/Mutations';
+import { MagicSpinner } from 'react-spinners-kit';
 import eye_logo from './img/eye_logo.png';
 
 let mailInput = true;
 
-const checkLogin = () =>{
+/* const checkLogin = () =>{
   if(!mailInput){
     return "/"
   }else{
     return "/Projects"
   }
-}
+} */
 
-const handleChange = (e) => {
-  mailInput = true;
-}
 
-const login = props => {
+class Login extends React.Component {
+
+  constructor(props, context){
+    super(props, context)
+
+    this.state = {
+      email: '',
+      pw: ''
+    }
+  }
+
+  handleChange(e) {
+    mailInput = true;
+
+    this.setState({
+      email: e.target.type === 'text' ? e.target.value : this.state.email,
+      pw: e.target.type === 'password' ? e.target.value : this.state.pw
+    })
+  }
 
   
-
+    render(){
   return (
     <div className="loginWrapper">
       <Grid className="Login">
@@ -33,29 +51,41 @@ const login = props => {
           }</code>
           </Col>
         </Row>
+        <Mutation mutation={LOGIN} onCompleted={() => this.props.history.push('/Projects')}>
+        {(login, { loading, error }) => (
+          <form onSubmit={e => {
+            e.preventDefault()
+            login({ variables: { data: { email: this.state.email, password: this.state.pw}}})
+          }}>
         <Row className="loginInner">
           <Col md={6} mdPush={6}>
-            <code>{<div className="loginInputLabel"><center><input type="text" placeholder="e-Mail"  onChange={(e)=>{handleChange(e)}}/></center> </div>}</code>
+            <code>{<div className="loginInputLabel"><center><input type="text" placeholder="e-Mail" value={this.state.email} onChange={(e)=>{this.handleChange(e)}}/></center> </div>}</code>
           </Col>
           <Col md={6} mdPull={6}>
-            <code>{<div className="loginInputLabel"> <center><input type="password" placeholder="password" /></center></div>}</code>
+            <code>{<div className="loginInputLabel"> <center><input type="password" placeholder="password" value={this.state.pw} onChange={(e)=>{this.handleChange(e)}} /></center></div>}</code>
           </Col>
         </Row>
         <center>
           <div className="buttonWrapper">
-            <NavLink to={checkLogin()} className="" onClick={(e) => {
+            {/* <NavLink to={checkLogin()} className="" onClick={(e) => {
               console.log('Login')
-            }}><center><button className="loginButton">Login</button></center></NavLink>
+            }}></NavLink> */}<center><button type="submit" className="loginButton">Login</button></center>
           </div>
           <div className="buttonWrapper">
             <NavLink to="/Register" className="" onClick={(e) => {
               console.log('Login')
-            }}><center><button className="registerButton">sign up</button></center></NavLink>
+            }}><center><button className="registerButton">sign up</button>
+                <MagicSpinner size={50} color="#6cd404" loading={loading} /></center></NavLink>
           </div>
         </center>
+        </form>
+        )}
+        </Mutation>
       </Grid>
     </div>
   );
-};
 
-export default login;
+};
+}
+
+export default Login;
