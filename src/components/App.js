a496b03fa2aceae0e512766718b33c8b0c0720cf
RopFoo <robert.pasdziernik@stud.hshl.de>
2019-01-09 19:14:54 +0100

    delete comment
    deleted dumb comments

--- src/components/App.js
+++ src/components/App.js
@@ -6,42 +6,6 @@ import "../public/styles/App.scss"
 
 import { LOGIN } from '../apollo/templates/Mutations';
 
-//DUMMYDB BENUTZEN
-/* 
-import users from '../dummyDB';
-
-DUMMYDB Benutzung z.b. so um das 2te Projekt zu fetchen:
-console.log(users[0].projects[1]);
-*/
-
-//TESTING A MUTATION - WORKS
-/* const signup = gql`
-  mutation {
-    login(data: { email: "riesen.bla@web.de", password: "123456789" }) {
-      token
-    }
-  }
-`
- */
- 
-//NOTE: LOGIN mit input variablen (TODO: benutzen von Mutation react-apollo Components wäre cooler)
-
-/* client
-  .mutate({
-    mutation: LOGIN,
-    variables: {
-      data:{
-        email: "riesen.bla@web.de",
-        password: "123456789"
-      }
-    }
-  })
-  .then(result => {
-    sessionStorage.setItem('token', result.data.login.token)
-    console.log(sessionStorage.getItem('token'));
-  }) */
-
-
 class App extends Component {
   render() {
     return (
